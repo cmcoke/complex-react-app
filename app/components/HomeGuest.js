761c193d64b91ca455e8de7e16f8c068eecd408f
cmcoke <minoru.cm@gmail.com>
2022-07-08 09:36:56 -0500

    allow user registration by send a user's username, email & password information to the MangoDB server.

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Page from "./Page";
+import Axios from "axios";
 
 const HomeGuest = () => {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  // sends the username, email & password values to the "MangoDB" database
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      /*
+      using the axios libary to send a post request to the "MangoDB" database
+
+       http://localhost:8080 -- is the url that the request is sent to
+
+       /register -- is the action that should be taken
+
+       { username, email, password } -- refers to the data that is sent to the server ( username, email, password -- are the names of the states as created in lines 6 - 8 )
+    */
+      await Axios.post("http://localhost:8080/register", { username, email, password });
+      console.log("User was successfully created");
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
   return (
     <Page wide={true} title={"Welcome !!!"}>
       <div className="row align-items-center">
@@ -10,24 +35,27 @@ const HomeGuest = () => {
           <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
-              <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+              {/* onChange={e => setUsername(e.target.value)} -- gets the input value and stores it into state */}
+              <input onChange={e => setUsername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
-              <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+              {/* onChange={e => setEmail(e.target.value)} -- gets the input value and stores it into state */}
+              <input onChange={e => setEmail(e.target.value)} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
-              <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+              {/* onChange={e => setPassword(e.target.value)} -- gets the input value and stores it into state */}
+              <input onChange={e => setPassword(e.target.value)} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
             </div>
             <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
               Sign up for ComplexApp
